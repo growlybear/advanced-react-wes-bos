7268a35af65c007b9e50c9323778ab2258967eb8
Michael Allan <Michael Allan>
2018-12-23 23:07:25 +1100

    Create custom Meta (Head) component

---
+++ sick-fits/frontend/components/Meta.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+const Meta = () => (
+  <Head>
+    <meta charSet="utf-8" />
+    <title>Sick Fits!</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="shortcut icon" href="/static/favicon.png" />
+    <link rel="stylesheet" type="text/css" href="/static/nprogress.css" />
+  </Head>
+)
+
+export default Meta
