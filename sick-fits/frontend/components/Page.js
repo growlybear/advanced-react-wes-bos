062fc03cefc910ead8073e030dd3c4d7f0d6de71
Michael Allan <Michael Allan>
2018-12-24 08:14:45 +1100

    Add styled-components to Page

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,16 +1,22 @@
 import React, { Component } from 'react'
+import styled from 'styled-components'
 
 import Meta from './Meta'
 import Header from './Header'
 
+const StyledPage = styled.div`
+  background: #fff;
+  color: #000;
+`
+
 class Page extends Component {
   render () {
     return (
-      <div>
+      <StyledPage>
         <Meta />
         <Header />
         { this.props.children }
-      </div>
+      </StyledPage>
     )
   }
 }
