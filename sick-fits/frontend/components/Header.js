affbfc6db5a1f962cb4b2db93e88657b5eecf00d
Michael Allan <Michael Allan>
2018-12-25 17:20:51 +1100

    Add nprogress loading progress bar

--- sick-fits/frontend/components/Header.js
+++ sick-fits/frontend/components/Header.js
@@ -1,7 +1,19 @@
 import Link from 'next/link'
 import styled from 'styled-components'
+import NProgress from 'nprogress'
+import Router from 'next/router'
 
 import Nav from './Nav'
+
+Router.onRouteChangeStart = () => {
+  NProgress.start()
+}
+Router.onRouteChangeComplete = () => {
+  NProgress.done()
+}
+Router.onRouteChangeError = () => {
+  NProgress.done()
+}
 
 const StyledHeader = styled.header`
   .bar {
