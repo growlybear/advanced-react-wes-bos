ff6235331a833fac55b7eb4463522320ac65ab48
Michael Allan <Michael Allan>
2018-12-27 08:30:35 +1100

    Use forwardTo from prisma-bindings, since schema query is identical to db's

--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,8 +1,11 @@
+const { forwardTo } = require('prisma-binding')
+
 const Query = {
-  async items(parent, args, ctx, info) {
-    const res = ctx.db.query.items()
-    return res
-  }
+  items: forwardTo('db')
+  // async items(parent, args, ctx, info) {
+  //   const res = ctx.db.query.items()
+  //   return res
+  // }
 };
 
 module.exports = Query;
