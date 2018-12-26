b7e007bcef7c6e855c6934824dc0cfbd6c50edd1
Michael Allan <Michael Allan>
2018-12-26 22:26:17 +1100

    Create createItem mutation on Prisma

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,10 +1,13 @@
 const Mutations = {
-  createDog(parent, args, ctx, info) {
-    // Temporaritly simulate db on the global object
-    global.dogs = global.dogs || []
-    const newDog = { name: args.name }
-    global.dogs.push(newDog)
-    return newDog
+  async createItem(parent, args, ctx, info) {
+    // TODO check it the user is logged in
+    const item = await ctx.db.mutation.createItem({
+      data: {
+        ...args
+      }
+    // NOTE need the info arg later when returning an item from the db
+    }, info)
+    return item
   }
 }
 
